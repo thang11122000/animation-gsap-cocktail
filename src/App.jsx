3c4deb443b6bf02c021863ec2543ad4bfac2f593
Doan Thang <thangnd111220@gmail.com>
2025-08-29 16:16:21 +0700

    Add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="min-h-[300vh]">
+      <Navbar></Navbar>
+    </main>
+  );
 };
 
 export default App;
