f4332bb356a9ae05b54d0d30c4c7855a55d554d8
Doan Thang <thangnd111220@gmail.com>
2025-08-29 15:29:27 +0700

    setup code

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
