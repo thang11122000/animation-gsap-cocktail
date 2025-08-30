3d41f19092cd358532164aa53fd98e0071489261
Doan Thang <thangnd111220@gmail.com>
2025-08-30 10:05:32 +0700

    Add Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <main className="min-h-[300vh]">
-      <Navbar></Navbar>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"></div>
     </main>
   );
 };
